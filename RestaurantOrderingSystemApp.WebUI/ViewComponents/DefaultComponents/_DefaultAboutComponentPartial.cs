069b1d48a235a0f87acd6436ad0c85f7510fafe8
tunc0687 <tunc0687@hotmail.com>
2024-05-16 01:57:06 +0300

    Update Web UI

--- RestaurantOrderingSystemApp.WebUI/ViewComponents/DefaultComponents/_DefaultAboutComponentPartial.cs
+++ RestaurantOrderingSystemApp.WebUI/ViewComponents/DefaultComponents/_DefaultAboutComponentPartial.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using RestaurantOrderingSystemApp.BusinessLayer.Abstract;
+using RestaurantOrderingSystemApp.WebUI.Dtos.AboutDtos;
 
 namespace RestaurantOrderingSystemApp.WebUI.ViewComponents.DefaultComponents
 {
-    public class _DefaultAboutComponentPartial(IAboutService _aboutService) : ViewComponent
+    public class _DefaultAboutComponentPartial(IAboutService _aboutService, IMapper _mapper) : ViewComponent
     {
         public IViewComponentResult InvokeAsync()
         {
-            var values = _aboutService.TGetListAll();
+            var values = _mapper.Map<List<ResultAboutDto>>(_aboutService.TGetListAll());
             if (values != null)
             {
                 return View(values);
--- RestaurantOrderingSystemApp.WebUI/ViewComponents/LayoutComponents/_LayoutNavbarComponentPartial.cs
+++ RestaurantOrderingSystemApp.WebUI/ViewComponents/LayoutComponents/_LayoutNavbarComponentPartial.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantOrderingSystemApp.BusinessLayer.Abstract;
+using RestaurantOrderingSystemApp.WebUI.Dtos.MessageDtos;
+using RestaurantOrderingSystemApp.WebUI.Dtos.NotificationDtos;
 
 namespace RestaurantOrderingSystemApp.WebUI.ViewComponents.LayoutComponents
 {
-    public class _LayoutNavbarComponentPartial : ViewComponent
+    public class _LayoutNavbarComponentPartial(INotificationService _notificationService, IMapper _mapper, IMessageService _messageService) : ViewComponent
     {
         public IViewComponentResult Invoke()
         {
+            var notificationCountByFalse = _notificationService.TNotificationCountByStatusFalse();
+            var messageCountByFalse = _messageService.TMessageCountByStatusFalse();
+
+            var notificationListByFalse = _mapper.Map<List<ResultNotificationDto>>(_notificationService.TGetAllNotificationByFalse());
+            var messageListByFalse = _mapper.Map<List<ResultMessageDto>>(_messageService.TGetAllMessageByFalse());
+
+            if (notificationListByFalse != null)
+            {
+                ViewBag.NCount = notificationCountByFalse;
+                ViewBag.MCount = messageCountByFalse;
+                return View(Tuple.Create(notificationListByFalse, messageListByFalse));
+            }
             return View();
         }
     }
